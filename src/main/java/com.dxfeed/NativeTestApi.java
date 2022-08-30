083b407979689170a1f3081fdacd8b24e850046a
Konstantin Ivaschenko <stdcion@gmail.com>
2022-08-30 17:42:14 +0300

    Add example with alloc unmanaged and pass to C

--- src/main/java/com.dxfeed/NativeTestApi.java
+++ src/main/java/com.dxfeed/NativeTestApi.java
@@ -1,14 +1,168 @@
 package com.dxfeed;
 
+import com.oracle.svm.core.c.ProjectHeaderFile;
 import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.UnmanagedMemory;
 import org.graalvm.nativeimage.c.CContext;
+import org.graalvm.nativeimage.c.constant.CEnum;
+import org.graalvm.nativeimage.c.constant.CEnumLookup;
+import org.graalvm.nativeimage.c.constant.CEnumValue;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.function.CFunctionPointer;
+import org.graalvm.nativeimage.c.function.InvokeCFunctionPointer;
+import org.graalvm.nativeimage.c.struct.*;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.PointerBase;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 @CContext(NativeTestApi.NativeTestApiDirectives.class)
 public class NativeTestApi {
     static class NativeTestApiDirectives implements CContext.Directives {
+        @Override
+        public List<String> getHeaderFiles() {
+            /*
+             * The header file with the C declarations that are imported. We use a helper class that
+             * locates the file in our project structure.
+             */
+            return Collections.singletonList(ProjectHeaderFile.resolve("com.dxfeed", "native/dxf_types.h"));
+        }
+    }
+
+
+    @CEnum("dxf_event_types")
+    enum EventsTypes {
+        dxf_event_type_quote,
+        dxf_event_type_time_and_sale;
+
+        @CEnumValue
+        public native int getCValue();
+
+        @CEnumLookup
+        public static native EventsTypes fromCValue(int value);
+    }
+
+    /**
+     * Base type of all events.
+     */
+    @CStruct("dxf_event")
+    interface EventNative extends PointerBase {
+        @CField("event_type")
+        int getEventType();
+
+        @CField("event_type")
+        void setEventType(int eventType);
+
+        @CField("symbol_name")
+        CCharPointer getSymbolName();
+
+        @CField("symbol_name")
+        void setSymbolName(CCharPointer symbolName);
+    }
+
+    /**
+     * Pointer to EventNative.
+     */
+    @CPointerTo(EventNative.class)
+    public interface EventNativePtr extends PointerBase {
+        /**
+         * Provides access to an array element by index.
+         *
+         * @param index The index of array.
+         * @return Returns pointer.
+         */
+        EventNativePtr addressOf(int index);
+
+        EventNative read();
+
+        void write(EventNative data);
+    }
+
+    /**
+     * The Quote.
+     */
+    @CStruct("dxf_event_quote")
+    interface QuoteNative extends EventNative {
+        @CField("bid_price")
+        double getBidPrice();
+
+        @CField("bid_price")
+        void setBidPrice(double bidPrice);
+
+        @CField("ask_price")
+        double getAskPrice();
+
+        @CField("ask_price")
+        void setAskPrice(double askPrice);
+    }
+
+    /**
+     * The TimeAndSale.
+     */
+    @CStruct("dxf_event_time_and_sale")
+    interface TimeAndSaleNative extends EventNative {
+        @CField("event_flag")
+        int getEventFlag();
+
+        @CField("event_flag")
+        void setEventFlag(int eventFlag);
+
+        @CField("index")
+        long getIndex();
+
+        @CField("index")
+        void setIndex(long index);
+    }
+
+    interface EventListenerFunctionPtr extends CFunctionPointer {
+        @InvokeCFunctionPointer
+        void invoke(EventNativePtr events, int count);
+    }
+
+    @CEntryPoint(name = "native_create_events_and_call_listener")
+    public static void createEventsAndCallListener(IsolateThread ignoreIsolate, EventListenerFunctionPtr listener) {
+        // Allocates an array of pointers.
+        var events = (EventNativePtr) UnmanagedMemory.calloc(SizeOf.get(EventNativePtr.class) * 2);
+
+        // Allocates Quote events.
+        var quote = (QuoteNative) UnmanagedMemory.calloc(SizeOf.get(QuoteNative.class));
+        quote.setEventType(EventsTypes.dxf_event_type_quote.getCValue());
+        quote.setSymbolName(allocCString("😁"));
+        quote.setAskPrice(555);
+        quote.setBidPrice(666);
+
+        // Allocates Quote events.
+        var tns = (TimeAndSaleNative) UnmanagedMemory.calloc(SizeOf.get(TimeAndSaleNative.class));
+        tns.setEventType(EventsTypes.dxf_event_type_time_and_sale.getCValue());
+        tns.setSymbolName(allocCString("ЁЙЪЬ"));
+        tns.setEventFlag(123);
+        tns.setIndex(456);
+
+        // Writes event pointers to array of pointers.
+        events.addressOf(0).write(quote);
+        events.addressOf(1).write(tns);
+
+        // Calls C listener.
+        listener.invoke(events, 2);
+
+        // Free all allocated memory.
+        UnmanagedMemory.free(tns.getSymbolName());
+        UnmanagedMemory.free(tns);
+        UnmanagedMemory.free(quote.getSymbolName());
+        UnmanagedMemory.free(quote);
+        UnmanagedMemory.free(events);
+    }
+
+    private static CCharPointer allocCString(String string) {
+        var bytes = string.getBytes(StandardCharsets.UTF_8);
+        var pointer = (CCharPointer) UnmanagedMemory.calloc(bytes.length + 1);
+        for (int i = 0; i < bytes.length; ++i) {
+            pointer.addressOf(i).write(bytes[i]);
+        }
+        return pointer;
     }
 
     /**
